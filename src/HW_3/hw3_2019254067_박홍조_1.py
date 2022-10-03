4983b5785948d2853a9ad48a6a94c2469bee8662
박홍조 <75837124+Hongjo-park@users.noreply.github.com>
2022-10-04 01:00:10 +0900

    Update hw3_2019254067_박홍조_1.py

---
+++ src/HW_3/hw3_2019254067_박홍조_1.py
@@ -0,0 +1,38 @@
+
+
+def dfs(r, c, idx, total):
+    global ans
+    if ans >= total + max_val * (3 - idx):
+        return
+    if idx == 3:
+        ans = max(ans, total)
+        return
+    else:
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < n and 0 <= nc < m and visit[nr][nc] == 0:
+                if idx == 1:
+                    visit[nr][nc] = 1
+                    dfs(r, c, idx + 1, total + array[nr][nc])
+                    visit[nr][nc] = 0
+                visit[nr][nc] = 1
+                dfs(nr, nc, idx + 1, total + array[nr][nc])
+                visit[nr][nc] = 0
+
+
+n, m = map(int, input().split())
+array = [list(map(int, input().split())) for _ in range(n)]
+visit = [([0] * m) for _ in range(n)]
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+ans = 0
+max_val = max(map(max, array))
+
+for r in range(n):
+    for c in range(m):
+        visit[r][c] = 1
+        dfs(r, c, 0, array[r][c])
+        visit[r][c] = 0
+
+print(ans)
