06e4c35c54465547a7fd88061631c43747d2e8f2
박홍조 <75837124+Hongjo-park@users.noreply.github.com>
2022-10-04 21:19:29 +0900

    hw3_update_dfs

--- src/HW_3/hw3_2019254067_박홍조_1.py
+++ src/HW_3/hw3_2019254067_박홍조_1.py
@@ -1,38 +1,57 @@
+# DFS로 2D 맵에서 주어진 블럭을 만들기
+# 1. ㅗ 모양은 시작 지점이 아닌 이미 한 번 탐색이 이루어졌을 때, 생성이 가능하다.
+# 2. 모든 블럭은 4칸을 차지한다. 
 
 
-def dfs(r, c, idx, total):
-    global ans
-    if ans >= total + max_val * (3 - idx):
-        return
+def is_run(row: int, col: int, visit_map: list) -> bool:
+    '''
+    방문하려는 곳의 row와 column이 0 이상이고 n과 m 사이에 있어야하며, 
+
+    방문 여부를 확인하는 2차원 list [n,m]에 방문한 적이 없어야한다.
+    '''
+    return True if 0 <= row < n and 0 <= col < m and visit_map[row][col] == 0 else False
+
+
+def dfs(row: int, col: int, idx: int, total: int) -> None:
+    global answer
+    # answer가 최대인지를 확인하고 Return
+    if answer >= total + max_val * (3 - idx):
+        return None
+
+    # 모든 블럭을 다 돈 경우 answer를 입력해준다. ( 이번 누적합 total과 이전 누적합 answer를 비교한다 )
     if idx == 3:
-        ans = max(ans, total)
-        return
+        answer = max(answer, total)
+        return None
     else:
         for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < n and 0 <= nc < m and visit[nr][nc] == 0:
+            nr = row + dr[i]
+            nc = col + dc[i]
+            if is_run(nr, nc, visit):
                 if idx == 1:
+                    # ㅗ 모양의 블럭 만들기 ( 다음 블럭을 탐색하는 것이 아닌 기존 블럭을 탐색합니다. )
                     visit[nr][nc] = 1
-                    dfs(r, c, idx + 1, total + array[nr][nc])
+                    dfs(row, col, idx + 1, total + array[nr][nc])
                     visit[nr][nc] = 0
                 visit[nr][nc] = 1
                 dfs(nr, nc, idx + 1, total + array[nr][nc])
                 visit[nr][nc] = 0
 
-
-n, m = map(int, input().split())
-array = [list(map(int, input().split())) for _ in range(n)]
-visit = [([0] * m) for _ in range(n)]
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-ans = 0
-max_val = max(map(max, array))
-
-for r in range(n):
-    for c in range(m):
-        visit[r][c] = 1
-        dfs(r, c, 0, array[r][c])
-        visit[r][c] = 0
-
-print(ans)
+if __name__ == '__main__':
+    # 입력
+    n, m = map(int, input().split())
+    array = [list(map(int, input().split())) for _ in range(n)]
+    # 0으로 채워진 n*m 2차원 List[int] 생성
+    visit = [([0] * m) for _ in range(n)]
+    # 이동을 결정할 direction list ( row, col )
+    dr = [-1, 0, 1, 0]
+    dc = [0, 1, 0, -1]
+    answer = 0
+    # 입력받은 array에서의 최대 Value
+    max_val = max(map(max, array))
+    # 탐색
+    for r in range(n):
+        for c in range(m):
+            visit[r][c] = 1
+            dfs(r, c, 0, array[r][c])
+            visit[r][c] = 0
+    print(answer)
--- src/HW_3/hw3_2019254067_박홍조_2.py
+++ src/HW_3/hw3_2019254067_박홍조_2.py
@@ -1,19 +1,18 @@
 
-n = int(input())
-sequence = list(map(int, input().split()))
-operators = list(map(int, input().split()))
-
+# [-10억 ~ 10억]까지에 대해서만 본다.
 maximum = -1e9
 minimum = 1e9
 
 
-def dfs(depth, total, plus, minus, multiply, divide):
+def dfs(depth: int, total: int, plus: int, minus: int, multiply: int, divide: int) -> None:
     global maximum, minimum
+    # depth는 n까지
     if depth == n:
         maximum = max(total, maximum)
         minimum = min(total, minimum)
-        return
+        return None
 
+    # + - * /에 따라서 계산한 후 탐색
     if plus:
         dfs(depth + 1, total + sequence[depth], plus - 1, minus, multiply, divide)
     if minus:
@@ -24,6 +23,13 @@ def dfs(depth, total, plus, minus, multiply, divide):
         dfs(depth + 1, int(total / sequence[depth]), plus, minus, multiply, divide - 1)
 
 
-dfs(1, sequence[0], operators[0], operators[1], operators[2], operators[3])
-print(maximum)
-print(minimum)
+if __name__ == '__main__':
+    # 입력
+    n = int(input())
+    # 수열
+    sequence = list(map(int, input().split()))
+    # 연산자
+    operators = list(map(int, input().split()))
+    dfs(1, sequence[0], operators[0], operators[1], operators[2], operators[3])
+    print(maximum)
+    print(minimum)
